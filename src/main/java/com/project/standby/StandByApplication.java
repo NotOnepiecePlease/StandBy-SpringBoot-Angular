f69a6ae096ddc7fa1049dd1fcdbb9ee8e49d1561
Adriano Andrade <66988844+NotOnepiecePlease@users.noreply.github.com>
2021-09-08 13:29:07 -0300

    Start Project
    - Projeto de estudo do sistema StandBy em SpringBoot, Angular e SQL.

---
+++ src/main/java/com/project/standby/StandByApplication.java
@@ -0,0 +1,13 @@
+package com.project.standby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandByApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandByApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/standby/StandByApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.standby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandByApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
